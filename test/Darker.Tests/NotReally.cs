abd33e2c4a1a7e1647ecc4750f1d156d84df79af
Daniel Stockhammer <daniel.stockhammer@huddle.com>
2016-07-28 10:21:03 +0100

    add test project

---
+++ test/Darker.Tests/NotReally.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace Darker.Tests
+{
+    public class NotReally
+    {
+        [Fact]
+        public void DoesntActuallyWork()
+        {
+            // todo
+            // can't have .net core unit tests for brighter, because polly doesn't support netcoreapp :(
+            // see https://github.com/App-vNext/Polly/issues/132
+            throw new NotImplementedException();
+        }
+    }
+}
