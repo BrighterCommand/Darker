fab6ccab4b422856a5ce4b45107b81f34b06f7e9
Daniel Stockhammer <daniel@stockhammer.it>
2017-02-06 21:01:47 +0000

    add simple injector integration

---
+++ src/Darker.SimpleInjector/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+#if !NETSTANDARD
+using System.Runtime.InteropServices;
+#endif
+
+[assembly: AssemblyCopyright("Copyright © Daniel Stockhammer 2017")]
+[assembly: AssemblyTrademark("Darker")]
+
+#if !NETSTANDARD
+[assembly: ComVisible(false)]
+[assembly: Guid("f0619a4f-381a-4a42-ad88-2726ba1fecc4")]
+#endif
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
