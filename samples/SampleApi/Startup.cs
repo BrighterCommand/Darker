e50dbf2044a13b72262ae770c1225bc6624f9d4d
Ian Cooper <ian.cooper@just-eat.com>
2020-07-01 15:13:32 +0100

    Move to netcoreapp3.1

--- samples/SampleApi/Startup.cs
+++ samples/SampleApi/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Paramore.Darker.AspNetCore;
 using Paramore.Darker.Policies;
 using Paramore.Darker.QueryLogging;
@@ -26,11 +28,11 @@ namespace SampleApi
                 .AddPolicies(ConfigurePolicies());
 
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
