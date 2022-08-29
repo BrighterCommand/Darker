96bf8528612d96fd51ea799f5e8717c7032e1772
Toby Henderson <hendersont@gmail.com>
2022-08-29 15:14:45 +0100

    feat(Examples): Add minimal Api example

---
+++ samples/SampleMinimalApi/DarkerSettings.cs
@@ -0,0 +1,39 @@
+﻿using Paramore.Darker.Policies;
+using Polly;
+using Polly.Registry;
+using SampleMinimalApi.Domain;
+
+namespace SampleMinimalApi;
+
+public class DarkerSettings
+{
+    public const string SomethingWentTerriblyWrongCircuitBreakerName = "SomethingWentTerriblyWrongCircuitBreaker";
+
+    public static IPolicyRegistry<string> ConfigurePolicies()
+    {
+        var defaultRetryPolicy = Policy
+            .Handle<Exception>()
+            .WaitAndRetryAsync(new[]
+            {
+                TimeSpan.FromMilliseconds(50),
+                TimeSpan.FromMilliseconds(100),
+                TimeSpan.FromMilliseconds(150)
+            });
+
+        var circuitBreakerPolicy = Policy
+            .Handle<Exception>()
+            .CircuitBreakerAsync(1, TimeSpan.FromMilliseconds(500));
+
+        var circuitBreakTheWorstCaseScenario = Policy
+            .Handle<SomethingWentTerriblyWrongException>()
+            .CircuitBreakerAsync(1, TimeSpan.FromSeconds(5));
+
+        var policyRegistry = new PolicyRegistry
+        {
+            {Constants.RetryPolicyName, defaultRetryPolicy},
+            {Constants.CircuitBreakerPolicyName, circuitBreakerPolicy},
+            {SomethingWentTerriblyWrongCircuitBreakerName, circuitBreakTheWorstCaseScenario}
+        };
+        return policyRegistry;
+    }
+}
