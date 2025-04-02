b0c7bc56c2a8bfe5abe2af0ef4daa3ca16bba4f8
Steve Bush <stevebu@bushchang.com>
2025-04-02 11:53:53 -0700

    MAUI TestApp

---
+++ SampleMauiTestApp/MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using Paramore.Darker.AspNetCore;
+using Paramore.Darker.Policies;
+using Paramore.Darker.QueryLogging;
+using SampleMauiTestApp.QueryHandlers;
+
+namespace SampleMauiTestApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureDarker()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+
+        public static MauiAppBuilder ConfigureDarker(this MauiAppBuilder appBuilder)
+        {
+            appBuilder.Services.AddDarker()
+                .AddHandlersFromAssemblies(typeof(GetPeopleQuery).Assembly)
+                .AddJsonQueryLogging()
+                .AddPolicies(DarkerSettings.ConfigurePolicies());
+
+            return appBuilder;
+        }
+    }
+}
